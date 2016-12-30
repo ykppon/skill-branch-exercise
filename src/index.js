903370fe6b55078c65a296b010148afb0d976c33
Степан Иванов <stepan_ivanov@192.168.0.2>
2016-12-30 12:19:40 +0300

    Решение задачи 2b

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ var app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    var name = req.query.fullname;
+    var nameList = name.split(' ');
+    var changeName = function(name){
+        var inic = '';
+        for(var i=0;i<nameList.length-1;i++) {
+            inic += ' '+nameList[i][0]+'.';
+        };
+        return inic;
+    };
+    var fig = nameList[nameList.length-1]+ changeName(name);
+    if(nameList.length>3||name==''||name.search(/\d/) != -1||name.search(/\_/) != -1||name.search(/\//) != -1){
+        fig = 'Invalid fullname';
+    };
+    res.send(fig);
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-})
+});
