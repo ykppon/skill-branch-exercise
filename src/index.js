e5a9efb4c69c779d21a184b44e4f8597522f84a1
Степан Иванов <stepan_ivanov@192.168.0.2>
2016-12-30 11:20:48 +0300

    Решение задачи 2а

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ var app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    var nums = req.query;
+    var a = 0;
+    var b = 0;
+    if(nums.a){a = nums.a};
+    if(nums.b){b = nums.b};
+    var summ = +a + +b;
+    res.send(summ.toString());
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
