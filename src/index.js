58cdce3f378060397aab47b847d62a46b1c757c7
Степан Иванов <ykppon@live.com>
2017-01-02 10:29:53 +0300

    Задание 2d 32 из ХЗ

--- src/index.js
+++ src/index.js
@@ -4,14 +4,47 @@ var app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-    var url = req.query.username;
-    var reg = new RegExp('@?(https?:)?(\/\/)?((telegram||vk||medium)[^\/]*\/)?@?([a-zA-Z0-9.\_]*)', 'i');
-    var result = url.match(reg);
+    var color = req.query.color;
+    function getHexRGBColor(color)
+    {
+        color = color.replace(/\s/g,"");
+        var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
 
+        if(aRGB)
+        {
+            color = '';
+            for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,'0$1');
+        }
+        else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, '$1$1$2$2$3$3');
+        return color;
+    }
+    if(color==undefined){
+        color = 'fuckthemall';
+    }
+    var reg = new RegExp(/^(\s|\u00A0)+/g);
+    var result = color.toLowerCase(color);
+    var trust = result.replace(reg, '');
+    color = result = result.replace(/#/, '');
+    result = '#' + result.replace(reg, '');
+    if(result.length==4){
+        result = '#';
+        for(var i=0; i<color.length;i++){
+            var prom = color[i]+color[i];
+            result += prom;
+        };
+    }else if(result.length!=7||result.search(/[g-zG-Z]/)!=-1){
+        result = 'Invalid color';
+    };
+    if(trust.substring(3,0)=='rgb'){
+        result = '#' + getHexRGBColor(trust);
+        if(result.length!=7){
+            result = 'Invalid color';
+        }
+    }
+    res.send(result);
 
-    res.send('@'+result[5]);
-})
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-})
+});
