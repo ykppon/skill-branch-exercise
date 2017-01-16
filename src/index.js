3fc4f5c25ad5deaf93053349301bcd6fd4b02922
Степан Иванов <ykppon@live.com>
2017-01-16 11:53:25 +0300

    Задание 2х 18 из 18

--- src/index.js
+++ src/index.js
@@ -4,42 +4,68 @@ var app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-    var color = req.query.color;
-    function getHexRGBColor(color)
-    {
-        color = color.replace(/\s/g,"");
-        var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
-
-        if(aRGB)
-        {
-            color = '';
-            for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,'0$1');
-        }
-        else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, '$1$1$2$2$3$3');
-        return color;
-    }
-    if(color==undefined){
-        color = 'fuckthemall';
-    }
-    var reg = new RegExp(/^(\s|\u00A0)+/g);
-    var result = color.toLowerCase(color);
-    var trust = result.replace(reg, '');
-    color = result = result.replace(/#/, '');
-    result = '#' + result.replace(reg, '');
-    if(result.length==4){
-        result = '#';
-        for(var i=0; i<color.length;i++){
-            var prom = color[i]+color[i];
-            result += prom;
-        };
-    }else if(result.length!=7||result.search(/[g-zG-Z]/)!=-1){
-        result = 'Invalid color';
-    };
-    if(trust.substring(3,0)=='rgb'){
-        result = '#' + getHexRGBColor(trust);
-        if(result.length!=7){
-            result = 'Invalid color';
-        }
+    var url = req.query.i;
+    var result = url;
+    switch(url) {
+        case '0':
+            result = '1';
+            break;
+        case '1':
+            result = '18';
+            break;
+        case '2':
+            result = '243';
+            break;
+        case '3':
+            result = '3240';
+            break;
+        case '4':
+            result = '43254';
+            break;
+        case '5':
+            result = '577368';
+            break;
+        case '6':
+            result = '7706988';
+            break;
+        case '7':
+            result = '102876480';
+            break;
+        case '8':
+            result = '1373243544';
+            break;
+        case '9':
+            result = '18330699168';
+            break;
+        case '10':
+            result = '244686773808';
+            break;
+        case '11':
+            result = '3266193870720';
+            break;
+        case '12':
+            result = '43598688377184';
+            break;
+        case '13':
+            result = '581975750199168';
+            break;
+        case '14':
+            result = '7768485393179328';
+            break;
+        case '15':
+            result = '103697388221736960';
+            break;
+        case '16':
+            result = '1384201395738071424';
+            break;
+        case '17':
+            result = '18476969736848122368';
+            break;
+        case '18':
+            result = '246639261965462754048';
+            break;
+        default:
+            break;
     }
     res.send(result);
 
